647c8d03ce41892a1fb0f3cae0380658ffc208e8
Ananthakrishnan <71790345+Ananthu26@users.noreply.github.com>
2024-12-31 12:08:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          accent: {
+            light: '#9D72FF',
+            DEFAULT: '#7C3AED',
+            dark: '#5B21B6'
+          },
+          dark: {
+            lighter: '#2D2D3A',
+            light: '#1F1F2C',
+            DEFAULT: '#151521',
+            dark: '#0C0C14'
+          },
+          light: {
+            DEFAULT: '#F8FAFC',
+            dark: '#E2E8F0'
+          }
+        },
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+          mono: ['JetBrains Mono', 'monospace']
+        }
+      }
+    }
+  }
